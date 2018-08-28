67f74feb0f6648171780790b0147c7695f14b0b2
voitov <tigran.voitov@spryker.com>
2018-08-28 20:39:58 +0300

    GLUE-394 added OrdersResourceRoutePlugin in glue dependency provider [skip ci]

---
+++ src/Spryker/Glue/OrdersRestApi/Dependency/Client/OrdersRestApiToSalesClientInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\OrdersRestApi\Dependency\Client;
+
+use Generated\Shared\Transfer\OrderListTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+
+interface OrdersRestApiToSalesClientInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return \Generated\Shared\Transfer\OrderListTransfer
+     */
+    public function getOrderListByCustomerReference(OrderTransfer $orderTransfer): OrderListTransfer;
+}
