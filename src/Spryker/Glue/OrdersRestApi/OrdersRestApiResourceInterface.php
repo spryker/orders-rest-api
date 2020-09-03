6fc99da41214b67942640ae7a065475de2a2e2a4
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-09-03 23:24:29 +0300

    CC-10190 Product bundles in orders.

--- src/Spryker/Glue/OrdersRestApi/OrdersRestApiResourceInterface.php
+++ src/Spryker/Glue/OrdersRestApi/OrdersRestApiResourceInterface.php
@@ -7,6 +7,24 @@
 
 namespace Spryker\Glue\OrdersRestApi;
 
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\RestOrderItemsAttributesTransfer;
+
 interface OrdersRestApiResourceInterface
 {
+    /**
+     * Specification:
+     * - Maps `ItemTransfer` to `RestOrderItemsAttributesTransfer`.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     * @param \Generated\Shared\Transfer\RestOrderItemsAttributesTransfer $restOrderItemsAttributesTransfer
+     *
+     * @return \Generated\Shared\Transfer\RestOrderItemsAttributesTransfer
+     */
+    public function mapItemTransferToRestOrderItemsAttributesTransfer(
+        ItemTransfer $itemTransfer,
+        RestOrderItemsAttributesTransfer $restOrderItemsAttributesTransfer
+    ): RestOrderItemsAttributesTransfer;
 }
